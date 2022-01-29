5bb4536d1157d62533ca8969c3349ccf63db31ff
Muhammed Rashid V <87640868+rashidrashiii@users.noreply.github.com>
2022-01-29 16:53:12 +0530

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
+from django.conf.urls import url
 
 from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
-    re_path(r'^media/(?P<path>.*)$',serve,{'document_root': settings.MEDIA_ROOT}),
-    re_path(r'^static/(?P<path>.*)$',serve,{'document_root': settings.STATIC_ROOT}),
+    url(r'^media/(?P<path>.*)$',serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$',serve,{'document_root': settings.STATIC_ROOT}),
 ]
